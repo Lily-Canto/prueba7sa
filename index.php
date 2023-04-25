35bdc8d5ee3ccfe7977d76383d97b86585c526a2
Lily-Canto <117927064+Lily-Canto@users.noreply.github.com>
2023-04-24 18:50:53 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="estilo.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<header>
+  <figure>
+    <img src="" alt="">
+  </figure>
+</header>
+
+<section>
+<article>
+  <a href="#">Inicio</a>
+  <a href="#">Quienes somos</a>
+  <a href="#">Servicio</a>
+  <a href="#">Ubicacion</a>
+  <a href="#">Contacto</a>
+</article>
+</section>
+
+<section>
+  <article>
+  <h1>
+    CONTACTO
+  </h1>
+
+  <form action="" name="form">
+    <label for="">NOMBRE</label>
+    <input type="text" name="nombre">
+
+    <label for="">APELLIDO</label>
+    <input type="text" name="apellido">
+
+    <label for="">MATRICULA</label>
+    <input type="text" name="matricula">
+
+    <label for="">CARRERA</label>
+    <input type="text" name="carrera">
+    
+    <input type="submit" value="enviar">
+  </form>
+  </article>
+</section>
+
+</body>
+</html>
